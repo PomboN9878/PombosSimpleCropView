48641cd61bd49357d46fcd60c9abd98c98604245
PomboN9878 <136838367+PomboN9878@users.noreply.github.com>
2025-12-02 16:49:41 -0300

    Gradle update

--- PomboCropView/build.gradle.kts
+++ PomboCropView/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 }
 
 android {
@@ -8,7 +9,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -22,17 +22,60 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    // Necessário para o JitPack funcionar corretamente
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+}
+
+// Configuração de publicação para JitPack
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.PomboN9878"
+                artifactId = "PombosSimpleCropView"
+                version = "1.0"
+
+                pom {
+                    name.set("PombosSimpleCropView")
+                    description.set("Simple and Minimalist Crop Image View")
+                    url.set("https://github.com/PomboN9878/PombosSimpleCropView")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("PomboN9878")
+                            name.set("PomboN9878")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
